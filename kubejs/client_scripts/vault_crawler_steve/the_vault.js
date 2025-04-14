16babfbed890196cbb766168f6a55880285f788f
Caigan <caiganmythfang@gmail.com>
2025-04-14 13:58:56 -0500

    TFB2 - Started Crawler Carl style item descriptions

---
+++ kubejs/client_scripts/vault_crawler_steve/the_vault.js
@@ -0,0 +1,197 @@
+onEvent('jei.information', event => {
+    event.add('the_vault:phoenix_feather', [
+        'Phoenix Capstone - Crystal Modifier',
+        'Combine with a Vault Crystal in a Crystal Workbench.',
+        'Adds Phoenix, which saves a User from death inside the Vault one time.',
+        'An extra life is a life saver! That is, unless you died in a bad spot. Cause this will respawn you RIGHT where you died! Good luck!"'
+    ])
+    event.add('the_vault:eye_of_avarice', [
+        'Avarice Capstone - Crystal Modifier',
+        'Combine with a Vault Crystal in a Crystal Workbench.',
+        'Adds Safe Zone, which negates all trap chances on chests.',
+        'It will NOT negate the chance of you making terrible tactical decisions, however.'
+    ])
+    event.add('the_vault:cheese_burger', [
+        'Cheese Burger - Vault Food',
+        'Consume to gain 800 Vault Experience.',
+        'Look at that, a bit of cheese and suddenly the burger is twice as good!'
+    ])
+    event.add('the_vault:double_cheese_burger', [
+        'Double Cheese Burger - Vault Food',
+        'Consume to gain 1,00 Vault Experience.',
+        'Whats better than one patty? Two of course. What, were you expecting a different ingredient than MORE meat?'
+    ])
+    event.add('the_vault:deluxe_cheese_burger', [
+        'Deluxe Cheese Burger - Vault Food',
+        'Consume to gain 2,000 Vault Experience.',
+        'How do you make meat and bread fancy? Well, other than cheese, you add a vegetable! Just not too many veggies. Not that fancy, okay?'
+    ])
+    event.add('the_vault:crispy_deluxe_cheese_burger', [
+        'Crispy Deluxe Cheese Burger - Vault Food',
+        'Consume to gain 3,200 Vault Experience.',
+        'Okay, now you can add more vegetables. Lettuce. Which probably has almost no nutrition, but it has a nice fancy crunch to it!'
+    ])
+    event.add('the_vault:salty_deluxe_cheese_burger', [
+        'Salty Deluxe Cheese Burger - Vault Food',
+        'Consume to gain 4,800 Vault Experience.',
+        'What if, instead of JUST another vegetable, you added a PICKLED vegetable instead?'
+    ])
+    event.add('the_vault:cheese_burger_feast', [
+        'Cheese Burger Feast - Vault Food',
+        'Consume to gain 7,200 Vault Experience.',
+        'Okay, really. The integrity of this burger is reaching critical mass, User. Adding this mix of ketchup and mayo is just going to make it easier to collapse when you try to bite into it.'
+    ])
+    event.add('the_vault:spicy_hearty_burger', [
+        'Spicy Hearty Burger - Vault Food',
+        'Consume to gain 60,000 Vault Experience.',
+        'So, not only will this burger fall to pieces the moment you take a bite of it, it`s also going to send you crying from the spiciest pepper known to existance. Good luck, I guess!'
+    ])
+    event.add('the_vault:burger_cheese', [
+        'Burger Cheese - Vault Food Ingredient',
+        'Used to make Vault Burgers.',
+        'Usually you want your cheese to be refridgerated. I`m pretty sure Living Chests are not chilled in any way whatsoever.'
+    ])
+    event.add('the_vault:burger_chili', [
+        'Burger Chili - Vault Food Ingredient',
+        'Used to make Vault Burgers.',
+        'Do you think that these are the hottest peppers ever because they absorb all the rage of the Vault Dwellers you kill? Just a thought, User!'
+    ])
+    event.add('the_vault:burger_lettuce', [
+        'Burger Lettuce - Vault Food Ingredient',
+        'Used to make Vault Burgers.',
+        'This lettuce has probably seen much better days. It`s still pretty crisp at least. Don`t think too hard about it.'
+    ])
+    event.add('the_vault:burger_pickles', [
+        'Burger Pickles - Vault Food Ingredient',
+        'Used to make Vault Burgers.',
+        'Pickled in the salt from a thousand Vault Dweller tears. Which actually, that sounds kind of disgusting, User. You sure you want to put this on a burger?'
+    ])
+    event.add('the_vault:burger_sauce', [
+        'Burger Sauce - Vault Food Ingredient',
+        'Used to make Vault Burgers.',
+        'The least kept secret of fast food restaurants: Fatty mayo and sugary ketchup, name a more iconic duo. Nevermind, you probably could.'
+    ])
+    event.add('the_vault:burger_tomato', [
+        'Burger Tomato - Vault Food Ingredient',
+        'Used to make Vault Burgers.',
+        'Grown by the thousands, only the most mediocre tomato specimins are brought into the Vaults. At least they bulk out the Burgers a bit, User.'
+    ])
+    event.add('the_vault:vault_rock', [
+        'Vault Rock - Vault Crystal Ingredient',
+        'The base for all Vault Crystal creations.',
+        'Easily found within Vault Stone, eventually you`ll find so many of these they`ll be overflowing your chests. Perhaps you can come up with some other uses for them, User?'
+    ])
+    event.add('the_vault:gem_pog', [
+        'POG - Vault Material',
+        'Used to make low-mid tier items.',
+        'A melding of all the Player Gems. Very POGgers, as the youth say. Wait, do they still say that?'
+    ])
+    event.add('the_vault:echo_pog', [
+        'Echo POG - Vault Material',
+        'Used to make mid-high tier items.',
+        'POG POG POg POg Pog Pog pog pog. Sorry, without giving different sized text, it`s hard to convey an echo in a text description. You`ll have to settle for that, User.'
+    ])
+    event.add('the_vault:vault_crystal', [
+        'Vault Crystal - Vault Portal Summoner',
+        'Used to create a portal into a Vault.',
+        'So many things went into making this little sparkling crystal. And you`re just gonna go in, ransack the place, and leave without looking back, huh?'
+    ])
+    event.add('the_vault:key_iskallium', [
+        'Gorginite Key - Vault Key',
+        'Used to open Gorginite Treasure Doors.',
+        'Treasure rooms are fantastic ways to get a bunch of items and materials...sometimes. At one point the Treasure Chest was replaced with a single item Pedestal, but then Users go angry. Go figure.'
+    ])
+    event.add('the_vault:key_gorginite', [
+        'Woldium Key - Vault Key',
+        'Used to open Woldium Treasure Doors.',
+        'Why the heck is this key at a 45 degree angle? What`s up with that?!'
+    ])
+    event.add('the_vault:key_sprarkletine', [
+        'Sparkletine Key - Vault Key',
+        'Used to open Sparkletine Treasure Doors.',
+        'Treasure rooms are fantastic ways to get a bunch of items and materials...sometimes. At one point the Treasure Chest was replaced with a single item Pedestal, but then Users got angry. Go figure.'
+    ])
+    event.add('the_vault:key_ashium', [
+        'Ashium Key - Vault Key',
+        'Used to open Ashium Treasure Doors.',
+        'Treasure rooms are fantastic ways to get a bunch of items and materials...sometimes. At one point the Treasure Chest was replaced with a single item Pedestal, but then Users got angry. Go figure.'
+    ])
+    event.add('the_vault:key_bomignite', [
+        'Bomignite Key - Vault Key',
+        'Used to open Bomignite Treasure Doors.',
+        'Treasure rooms are fantastic ways to get a bunch of items and materials...sometimes. At one point the Treasure Chest was replaced with a single item Pedestal, but then Users got angry. Go figure.'
+    ])
+    event.add('the_vault:key_tubium', [
+        'Tubium Key - Vault Key',
+        'Used to open Tubium Treasure Doors.',
+        'Treasure rooms are fantastic ways to get a bunch of items and materials...sometimes. At one point the Treasure Chest was replaced with a single item Pedestal, but then Users got angry. Go figure.'
+    ])
+    event.add('the_vault:key_upaline', [
+        'Upaline Key - Vault Key',
+        'Used to open Upaline Treasure Doors.',
+        'Treasure rooms are fantastic ways to get a bunch of items and materials...sometimes. At one point the Treasure Chest was replaced with a single item Pedestal, but then Users got angry. Go figure.'
+    ])
+    event.add('the_vault:key_petezanite', [
+        'Petezanite Key - Vault Key',
+        'Used to open Petezanite Treasure Doors.',
+        'Treasure rooms are fantastic ways to get a bunch of items and materials...sometimes. At one point the Treasure Chest was replaced with a single item Pedestal, but then Users got angry. Go figure.'
+    ])
+    event.add('the_vault:key_xeenium', [
+        'Xeenium Key - Vault Key',
+        'Used to open Xeenium Treasure Doors.',
+        'Treasure rooms are fantastic ways to get a bunch of items and materials...sometimes. At one point the Treasure Chest was replaced with a single item Pedestal, but then Users got angry. Go figure.'
+    ])
+    event.add('the_vault:antique', [
+        'Antique Stamp - Vault Collectable',
+        'Collect for special prizes!',
+        'Ever hear old folks talk about collecting stamps and buying penny candy at the soda fountain? Well, we can guarantee that your nana never had to do the things you need to do inside a Vault to find these rare collectables!',
+        'Each of these Antiques drops under certain conditions. What are they, you ask? You figure it out!'
+    ])
+    event.add('the_vault:antique_collector_book', [
+        'Antique Stamp Collector Book - Collection Storage',
+        'Used to open Xeenium Treasure Doors.',
+        'An old book to put your old stamps into so they don`t clutter up your other other storage. At least it`s all in one place.'
+    ])
+    event.add('the_vault:unidentified_artifact', [
+        'Unidentfied Artifact - Artifact',
+        'Use to discover one of the Artifacts.',
+        'This is the entire reason why you are here, User. Find the artifacts. All 25 of them!'
+    ])
+    event.add('the_vault:augment', [
+        'Augment - Vault Crystal Modifier.',
+        'Combining a Vault Crystal with an Augment in an anvil will guarantee the theme that is specified on the Augment.',
+        'Sometimes the themes in the Vaults can be quite painful. Bright eye searing pastels? Blinding snow? Berry bushes everywhere that stab you? We don`t blame ya for wanting to guarantee a nice looking vault! '
+    ])
+    event.add('the_vault:vault_catalyst_fragment', [
+        'Catalyst Fragment - Crafting Component',
+        'Can be found inside Wooden Chests in unmodified Vaults starting at Level 24.'
+    ])
+    event.add('the_vault:vault_charm', [
+        'Vault Charm - Charm',
+        'If you have this charm in your inventory, any item you set in the Junk Controller will be immediately destroyed if you pick it up.',
+        'If you don`t have this charm in your inventory, hope you like your inventory constantly being filled with absolute trash!',
+    ])
+    event.add('the_vault:vault_charm_controller', [
+        'Junk Controller - Device',
+        'The Junk Controller lets you set a list of items that will be destroyed upon pickup while you are inside a Vault.',
+        'You can increase the number of items the Junk Controller can destroy with Vault Junk Upgrades.',
+        'While the bigger Upgrades are quite expensive, can you REALLY put a price on having more loot and less trash in your inventory?'
+    ])
+    event.add('the_vault:cryo_chamber', [
+        'Cry Chamber - Legacy Device',
+        'This device was used to create Eternals!',
+        'Yes, was! Eternals have been disabled for a rework, but this device is still available to make if you like throwing materials away at strange looking machines!'
+    ])
+    event.add('the_vault:vault_altar', [
+        'Vault Altar - Device',
+        'Enables you to create Vault Crystals out of Vault Rocks. Placing one of a variety of Vault Rocks on the Altar will show you a list of materials needed to create the Crystal.',
+        'Once the material cost has been paid, apply a Redstone Signal with a button or lever to create the Vault Crystal',
+        'Do note as you make more crystals, the costs will go up and the variety of materials will expand. Also, as you level up, the variety of materials will expand more. Also also, as you Research mods, the variety of materials will expand even more!'
+    ])
+    event.add('the_vault:soul_flame', [
+        'Soul Flame - Crystal Modifier',
+        'Combine with a Vault Crystal to add Afterlife.',
+        'Afterlife prevents loss of items upon death inside the Vault, but also sets Experience gained to 50% and may curse your Vault Crystal',
+        'Sometimes, the Vaults kick your tail. And then keeps kicking it. And you can only keep so many sets of equipment before your place looks like an armory for an entire country. Use the Soul Flame to get back on your feet, bud!'
+    ])
+})
