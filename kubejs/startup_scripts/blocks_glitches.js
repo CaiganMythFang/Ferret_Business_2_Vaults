90f77ce267e194463ebbd14ceda00cb60175ef63
Caigan <caiganmythfang@gmail.com>
2025-11-09 11:34:00 -0600

    KubeJS - Added 8 Glitched Blocks

---
+++ kubejs/startup_scripts/blocks_glitches.js
@@ -0,0 +1,127 @@
+// Glitched Vault Cobble
+onEvent('block.registry', event => {
+  event.create('tfb2:glitched_vault_cobble')
+  	   .material('dragon_egg')
+       .hardness(0.5)
+       .displayName('Glitched Vault Cobblestone') 
+       .tagItem('tfb2:glitched_block')
+  })
+  
+onEvent('item.tooltip', tooltip => {
+tooltip.addAdvanced('tfb2:glitched_vault_cobble', (item, advanced, text) => {
+    text.add(1, [Text.of('A').obfuscated(), Text.of('Glitched').lightPurple(), Text.of('A').obfuscated()])
+    text.add(2, [Text.of('A glitched block, useful to The Ferret Business').darkPurple()])
+    })
+});
+
+// Glitched Deepslate Cobble
+onEvent('block.registry', event => {
+  event.create('tfb2:glitched_deepslate_cobble')
+  	   .material('dragon_egg')
+       .hardness(0.5)
+       .displayName('Glitched Deepslate Cobblestone') 
+       .tagItem('tfb2:glitched_block')
+  })
+  
+onEvent('item.tooltip', tooltip => {
+tooltip.addAdvanced('tfb2:glitched_deepslate_cobble', (item, advanced, text) => {
+    text.add(1, [Text.of('A').obfuscated(), Text.of('Glitched').lightPurple(), Text.of('A').obfuscated()])
+    text.add(2, [Text.of('A glitched block, useful to The Ferret Business').darkPurple()])
+    })
+});
+
+// Glitched Dark Oak Log
+onEvent('block.registry', event => {
+  event.create('tfb2:glitched_dark_oak_log')
+  	   .material('dragon_egg')
+       .hardness(0.5)
+       .displayName('Glitched Dark Oak Log') 
+       .tagItem('tfb2:glitched_block')
+  })
+  
+onEvent('item.tooltip', tooltip => {
+tooltip.addAdvanced('tfb2:glitched_dark_oak_log', (item, advanced, text) => {
+    text.add(1, [Text.of('A').obfuscated(), Text.of('Glitched').lightPurple(), Text.of('A').obfuscated()])
+    text.add(2, [Text.of('A glitched block, useful to The Ferret Business').darkPurple()])
+    })
+});
+
+// Glitched Sandstone
+onEvent('block.registry', event => {
+  event.create('tfb2:glitched_sandstone')
+  	   .material('dragon_egg')
+       .hardness(0.5)
+       .displayName('Glitched Sandstone') 
+       .tagItem('tfb2:glitched_block')
+  })
+  
+onEvent('item.tooltip', tooltip => {
+tooltip.addAdvanced('tfb2:glitched_sandstone', (item, advanced, text) => {
+    text.add(1, [Text.of('A').obfuscated(), Text.of('Glitched').lightPurple(), Text.of('A').obfuscated()])
+    text.add(2, [Text.of('A glitched block, useful to The Ferret Business').darkPurple()])
+    })
+});
+
+// Glitched Snow Block
+onEvent('block.registry', event => {
+  event.create('tfb2:glitched_snow_block')
+  	   .material('dragon_egg')
+       .hardness(0.5)
+       .displayName('Glitched Snow Block') 
+       .tagItem('tfb2:glitched_block')
+  })
+  
+onEvent('item.tooltip', tooltip => {
+tooltip.addAdvanced('tfb2:glitched_snow_block', (item, advanced, text) => {
+    text.add(1, [Text.of('A').obfuscated(), Text.of('Glitched').lightPurple(), Text.of('A').obfuscated()])
+    text.add(2, [Text.of('A glitched block, useful to The Ferret Business').darkPurple()])
+    })
+});
+
+// Glitched Purple Terracotta
+onEvent('block.registry', event => {
+  event.create('tfb2:glitched_purple_terracotta')
+  	   .material('dragon_egg')
+       .hardness(0.5)
+       .displayName('Glitched Snow Block') 
+       .tagItem('tfb2:glitched_block')
+  })
+  
+onEvent('item.tooltip', tooltip => {
+tooltip.addAdvanced('tfb2:glitched_purple_terracotta', (item, advanced, text) => {
+    text.add(1, [Text.of('A').obfuscated(), Text.of('Glitched').lightPurple(), Text.of('A').obfuscated()])
+    text.add(2, [Text.of('A glitched block, useful to The Ferret Business').darkPurple()])
+    })
+});
+
+// Glitched Dirt
+onEvent('block.registry', event => {
+  event.create('tfb2:glitched_dirt')
+  	   .material('dragon_egg')
+       .hardness(0.5)
+       .displayName('Glitched Dirt') 
+       .tagItem('tfb2:glitched_block')
+  })
+  
+onEvent('item.tooltip', tooltip => {
+tooltip.addAdvanced('tfb2:glitched_dirt', (item, advanced, text) => {
+    text.add(1, [Text.of('A').obfuscated(), Text.of('Glitched').lightPurple(), Text.of('A').obfuscated()])
+    text.add(2, [Text.of('A glitched block, useful to The Ferret Business').darkPurple()])
+    })
+});
+
+// Glitched Crimsite
+onEvent('block.registry', event => {
+  event.create('tfb2:glitched_crimsite')
+  	   .material('dragon_egg')
+       .hardness(0.5)
+       .displayName('Glitched Crimsite') 
+       .tagItem('tfb2:glitched_block')
+  })
+  
+onEvent('item.tooltip', tooltip => {
+tooltip.addAdvanced('tfb2:glitched_crimsite', (item, advanced, text) => {
+    text.add(1, [Text.of('A').obfuscated(), Text.of('Glitched').lightPurple(), Text.of('A').obfuscated()])
+    text.add(2, [Text.of('A glitched block, useful to The Ferret Business').darkPurple()])
+    })
+});
