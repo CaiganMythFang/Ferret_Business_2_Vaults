03408d2b7d4ff5ad62e1467530db56dfe969bb6a
Caigan <caiganmythfang@gmail.com>
2025-11-21 10:05:17 -0600

    Added Auxillary Blocks Blind Boxes

--- kubejs/server_scripts/blindboxes/blindbox_a_man_with_plushies.js
+++ kubejs/server_scripts/blindboxes/blindbox_a_man_with_plushies.js
@@ -1,9 +1,9 @@
 onEvent('item.right_click', event => {
 	
-  if (event.item.id == 'tfb2:blindbox_a_man_with_plushies_cheap') {
+  if (event.item.id == 'tfb2:blindbox_auxiliary_blocks_cheap') {
 	  
-	event.server.runCommandSilent('loot give ' + event.getEntity() + ' loot tfb2:a_man_with_plushies/a_man_with_plushies_cheap');
-	event.player.tell(Text.lightPurple("You open up an Chinchilla Golf Blind Box!"));
+	event.server.runCommandSilent('loot give ' + event.getEntity() + ' loot tfb2:auxiliary_blocks/auxiliary_blocks_cheap');
+	event.player.tell(Text.lightPurple("You open up an Aardvark Gap Blind Box!"));
 	  
 	if (!event.player.isCreativeMode()) { //if creative mode dont consume; not necessary
 	  event.item.setCount(event.item.getCount() - 1)
---
+++ kubejs/startup_scripts/blindboxes/blindbox_auxiliary_blocks.js
@@ -0,0 +1,18 @@
+// a_man_with_plushies Blind Box
+// cheap
+onEvent('item.registry', event => {
+	event.create('tfb2:blindbox_auxiliary_blocks_cheap')
+		.displayName('Cheap Blind Box - Aardvark Gap')
+		.maxStackSize(64)
+		.rarity(RARITY_COMMON)
+		.group('tfb2')
+});
+
+onEvent('item.tooltip', tooltip => {
+	tooltip.addAdvanced('tfb2:blindbox_auxiliary_blocks_cheap', (item, advanced, text) => {
+		text.add(1, [Text.of('A Ferret Business Blind Box! Right Click it to see what you get!').lightPurple()])
+		text.add(2, [Text.of('Cheap Blind Boxes have a tiny chance of containing ANY item!').darkPurple()])
+	})
+});
+
+
