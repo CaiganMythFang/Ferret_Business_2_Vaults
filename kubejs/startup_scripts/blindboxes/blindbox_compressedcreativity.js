07e6f2488cb20e0d80d888f818e2abf7962d8168
Caigan <caiganmythfang@gmail.com>
2026-03-24 23:21:28 -0500

    KubeJS - Updated BlindBox script

---
+++ kubejs/startup_scripts/blindboxes/blindbox_compressedcreativity.js
@@ -0,0 +1,33 @@
+// animalistic_a Blind Box
+// cheap
+onEvent('item.registry', event => {
+	event.create('tfb2:blindbox_compressedcreativity_cheap')
+		.displayName('Cheap Blind Box - Fennec Drive')
+		.maxStackSize(64)
+		.rarity(RARITY_COMMON)
+		.group('tfb2')
+});
+
+onEvent('item.tooltip', tooltip => {
+	tooltip.addAdvanced('tfb2:blindbox_compressedcreativity_cheap', (item, advanced, text) => {
+		text.add(1, [Text.of('A Ferret Business Blind Box! Right Click it to see what you get!').lightPurple()])
+		text.add(2, [Text.of('Cheap Blind Boxes have a tiny chance of containing ANY item!').darkPurple()])
+	})
+});
+
+// animalistic_a Blind Box
+// decent
+onEvent('item.registry', event => {
+	event.create('tfb2:blindbox_compressedcreativity_decent')
+		.displayName('Decent Blind Box - Wicker Skate')
+		.maxStackSize(64)
+		.rarity(RARITY_UNCOMMON)
+		.group('tfb2')
+});
+
+onEvent('item.tooltip', tooltip => {
+	tooltip.addAdvanced('tfb2:blindbox_compressedcreativity_decent', (item, advanced, text) => {
+		text.add(1, [Text.of('A Ferret Business Blind Box! Right Click it to see what you get!').lightPurple()])
+		text.add(2, [Text.of('Decent Blind Boxes have either Decent or Good items!').darkPurple()])
+	})
+});
