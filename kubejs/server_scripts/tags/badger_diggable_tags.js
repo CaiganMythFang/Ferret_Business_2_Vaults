ce8abe04359c434a9c890e0b162b515b16646a93
Caigan <caiganmythfang@gmail.com>
2026-05-25 11:45:54 -0500

    Origins - Added more blocks to badger_diggable tag

---
+++ kubejs/server_scripts/tags/badger_diggable_tags.js
@@ -0,0 +1,9 @@
+
+onEvent("item.tags", event => {
+    event.add('origins:badger_diggable', '#minecraft:dirt')
+    event.add('origins:badger_diggable', '#chipped:dirt')
+    event.add('origins:badger_diggable', '#tfb2:dripstone_block')
+    event.add('origins:badger_diggable', '#forge:cobblestone')
+    event.add('origins:badger_diggable', '#forge:stone')
+    event.add('origins:badger_diggable', '#forge:sandstone')
+})
