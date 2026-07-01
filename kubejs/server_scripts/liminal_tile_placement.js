fb97bb7ace4662c25a4ceb042eead2913ffcf5ee
Caigan <caiganmythfang@gmail.com>
2026-07-01 11:25:53 -0500

    Liminal Mall - Lowered max Liminal Tile placement check

--- kubejs/server_scripts/liminal_tile_placement.js
+++ kubejs/server_scripts/liminal_tile_placement.js
@@ -1,8 +1,8 @@
 onEvent('block.right_click', event => {
     if(event.item.id != 'tfb2:liminal_tile') return
-    if(event.block.y >= 73 && event.block.y <= 80) return
+    if(event.block.y >= 73 && event.block.y <= 78) return
     else {
-        event.player.tell('The Liminal Tile can only be placed between Y level 73 and 80')
+        event.player.tell('The Liminal Tile can only be placed between Y level 73 and 78')
         event.cancel()
     }
 });
