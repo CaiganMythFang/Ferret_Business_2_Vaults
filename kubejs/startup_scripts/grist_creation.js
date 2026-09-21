07b52056b27e84866033659aabc5e825dfbdee75
Caigan <caiganmythfang@gmail.com>
2026-09-21 13:45:30 -0500

    Grist Alchemy - Part 2 of Compressed Grist Candy files

--- kubejs/startup_scripts/grist_creation.js
+++ kubejs/startup_scripts/grist_creation.js
@@ -1,5 +1,6 @@
 onEvent("item.registry", event => {
     event.create('tfb2:grist_candy_bag')
+    
     event.create('kubejs:chroma_gum_drop').food(food => {
         food
             .hunger(1)
@@ -7,6 +8,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_chroma_gum_drop_1x')
+    event.create('kubejs:compressed_chroma_gum_drop_2x')
+    event.create('kubejs:compressed_chroma_gum_drop_3x')
+    event.create('kubejs:compressed_chroma_gum_drop_4x')
+    
     event.create('kubejs:carbs_marshmallow').food(food => {
         food
             .hunger(1)
@@ -14,6 +20,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_carbs_marshmallow_1x')
+    event.create('kubejs:compressed_carbs_marshmallow_2x')
+    event.create('kubejs:compressed_carbs_marshmallow_3x')
+    event.create('kubejs:compressed_carbs_marshmallow_4x')
+    
     event.create('kubejs:scrappy_lolly').food(food => {
         food
             .hunger(1)
@@ -21,6 +32,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_scrappy_lolly_1x')
+    event.create('kubejs:compressed_scrappy_lolly_2x')
+    event.create('kubejs:compressed_scrappy_lolly_3x')
+    event.create('kubejs:compressed_scrappy_lolly_4x')
+    
     event.create('kubejs:vitamin_v_jellybean').food(food => {
         food
             .hunger(1)
@@ -28,6 +44,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_vitamin_v_jellybean_1x')
+    event.create('kubejs:compressed_vitamin_v_jellybean_2x')
+    event.create('kubejs:compressed_vitamin_v_jellybean_3x')
+    event.create('kubejs:compressed_vitamin_v_jellybean_4x')
+    
     event.create('kubejs:rainbownium_twist_candy').food(food => {
         food
             .hunger(1)
@@ -35,6 +56,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_rainbownium_twist_candy_1x')
+    event.create('kubejs:compressed_rainbownium_twist_candy_2x')
+    event.create('kubejs:compressed_rainbownium_twist_candy_3x')
+    event.create('kubejs:compressed_rainbownium_twist_candy_4x')
+    
     event.create('kubejs:ego_chocolate_truffle').food(food => {
         food
             .hunger(1)
@@ -42,6 +68,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_ego_chocolate_truffle_1x')
+    event.create('kubejs:compressed_ego_chocolate_truffle_2x')
+    event.create('kubejs:compressed_ego_chocolate_truffle_3x')
+    event.create('kubejs:compressed_ego_chocolate_truffle_4x')
+    
     event.create('kubejs:morkite_sucker').food(food => {
         food
             .hunger(1)
@@ -49,6 +80,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_morkite_sucker_1x')
+    event.create('kubejs:compressed_morkite_sucker_2x')
+    event.create('kubejs:compressed_morkite_sucker_3x')
+    event.create('kubejs:compressed_morkite_sucker_4x')
+    
     event.create('kubejs:boxium_chocolate_square').food(food => {
         food
             .hunger(1)
@@ -56,6 +92,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_boxium_chocolate_square_1x')
+    event.create('kubejs:compressed_boxium_chocolate_square_2x')
+    event.create('kubejs:compressed_boxium_chocolate_square_3x')
+    event.create('kubejs:compressed_boxium_chocolate_square_4x')
+    
     event.create('kubejs:deserialized_mint').food(food => {
         food
             .hunger(1)
@@ -63,6 +104,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_deserialized_mint_1x')
+    event.create('kubejs:compressed_deserialized_mint_2x')
+    event.create('kubejs:compressed_deserialized_mint_3x')
+    event.create('kubejs:compressed_deserialized_mint_4x')
+    
     event.create('kubejs:the_world_jawbreaker').food(food => {
         food
             .hunger(1)
@@ -70,6 +116,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_the_world_jawbreaker_1x')
+    event.create('kubejs:compressed_the_world_jawbreaker_2x')
+    event.create('kubejs:compressed_the_world_jawbreaker_3x')
+    event.create('kubejs:compressed_the_world_jawbreaker_4x')
+    
     event.create('kubejs:junkite_hard_candy').food(food => {
         food
             .hunger(1)
@@ -77,6 +128,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_junkite_hard_candy_1x')
+    event.create('kubejs:compressed_junkite_hard_candy_2x')
+    event.create('kubejs:compressed_junkite_hard_candy_3x')
+    event.create('kubejs:compressed_junkite_hard_candy_4x')
+    
     event.create('kubejs:fopium_chocolate_coin').food(food => {
         food
             .hunger(1)
@@ -84,6 +140,11 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_fopium_chocolate_coin_1x')
+    event.create('kubejs:compressed_fopium_chocolate_coin_2x')
+    event.create('kubejs:compressed_fopium_chocolate_coin_3x')
+    event.create('kubejs:compressed_fopium_chocolate_coin_4x')
+    
     event.create('kubejs:greed_filled_bonbon').food(food => {
         food
             .hunger(1)
@@ -91,6 +152,101 @@ onEvent("item.registry", event => {
             .alwaysEdible()
             .fastToEat()
     })
+    event.create('kubejs:compressed_greed_filled_bonbon_1x')
+    event.create('kubejs:compressed_greed_filled_bonbon_2x')
+    event.create('kubejs:compressed_greed_filled_bonbon_3x')
+    event.create('kubejs:compressed_greed_filled_bonbon_4x')
+    
+    event.create('kubejs:compressed_shale_peep_1x')
+    event.create('kubejs:compressed_shale_peep_2x')
+    event.create('kubejs:compressed_shale_peep_3x')
+    event.create('kubejs:compressed_shale_peep_4x')
+    
+    event.create('kubejs:compressed_rust_gummy_eye_1x')
+    event.create('kubejs:compressed_rust_gummy_eye_2x')
+    event.create('kubejs:compressed_rust_gummy_eye_3x')
+    event.create('kubejs:compressed_rust_gummy_eye_4x')
+    
+    event.create('kubejs:compressed_ruby_lollipop_1x')
+    event.create('kubejs:compressed_ruby_lollipop_2x')
+    event.create('kubejs:compressed_ruby_lollipop_3x')
+    event.create('kubejs:compressed_ruby_lollipop_4x')
+    
+    event.create('kubejs:compressed_quartz_jelly_bean_1x')
+    event.create('kubejs:compressed_quartz_jelly_bean_2x')
+    event.create('kubejs:compressed_quartz_jelly_bean_3x')
+    event.create('kubejs:compressed_quartz_jelly_bean_4x')
+    
+    event.create('kubejs:compressed_marble_jawbreaker_1x')
+    event.create('kubejs:compressed_marble_jawbreaker_2x')
+    event.create('kubejs:compressed_marble_jawbreaker_3x')
+    event.create('kubejs:compressed_marble_jawbreaker_4x')
+    
+    event.create('kubejs:compressed_iodine_licorice_1x')
+    event.create('kubejs:compressed_iodine_licorice_2x')
+    event.create('kubejs:compressed_iodine_licorice_3x')
+    event.create('kubejs:compressed_iodine_licorice_4x')
+    
+    event.create('kubejs:compressed_gold_candy_ribbon_1x')
+    event.create('kubejs:compressed_gold_candy_ribbon_2x')
+    event.create('kubejs:compressed_gold_candy_ribbon_3x')
+    event.create('kubejs:compressed_gold_candy_ribbon_4x')
+    
+    event.create('kubejs:compressed_diamond_mint_1x')
+    event.create('kubejs:compressed_diamond_mint_2x')
+    event.create('kubejs:compressed_diamond_mint_3x')
+    event.create('kubejs:compressed_diamond_mint_4x')
+    
+    event.create('kubejs:compressed_chalk_candy_cigarette_1x')
+    event.create('kubejs:compressed_chalk_candy_cigarette_2x')
+    event.create('kubejs:compressed_chalk_candy_cigarette_3x')
+    event.create('kubejs:compressed_chalk_candy_cigarette_4x')
+    
+    event.create('kubejs:compressed_caulk_pretzel_1x')
+    event.create('kubejs:compressed_caulk_pretzel_2x')
+    event.create('kubejs:compressed_caulk_pretzel_3x')
+    event.create('kubejs:compressed_caulk_pretzel_4x')
+    
+    event.create('kubejs:compressed_build_gushers_1x')
+    event.create('kubejs:compressed_build_gushers_2x')
+    event.create('kubejs:compressed_build_gushers_3x')
+    event.create('kubejs:compressed_build_gushers_4x')
+    
+    event.create('kubejs:compressed_artifact_warhead_1x')
+    event.create('kubejs:compressed_artifact_warhead_2x')
+    event.create('kubejs:compressed_artifact_warhead_3x')
+    event.create('kubejs:compressed_artifact_warhead_4x')
+    
+    event.create('kubejs:compressed_amethyst_hard_candy_1x')
+    event.create('kubejs:compressed_amethyst_hard_candy_2x')
+    event.create('kubejs:compressed_amethyst_hard_candy_3x')
+    event.create('kubejs:compressed_amethyst_hard_candy_4x')
+    
+    event.create('kubejs:compressed_amber_gummy_worm_1x')
+    event.create('kubejs:compressed_amber_gummy_worm_2x')
+    event.create('kubejs:compressed_amber_gummy_worm_3x')
+    event.create('kubejs:compressed_amber_gummy_worm_4x')
+    
+    event.create('kubejs:compressed_zillium_skittles_1x')
+    event.create('kubejs:compressed_zillium_skittles_2x')
+    event.create('kubejs:compressed_zillium_skittles_3x')
+    event.create('kubejs:compressed_zillium_skittles_4x')
+    
+    event.create('kubejs:compressed_uranium_gummy_bear_1x')
+    event.create('kubejs:compressed_uranium_gummy_bear_2x')
+    event.create('kubejs:compressed_uranium_gummy_bear_3x')
+    event.create('kubejs:compressed_uranium_gummy_bear_4x')
+    
+    event.create('kubejs:compressed_tar_black_licorice_1x')
+    event.create('kubejs:compressed_tar_black_licorice_2x')
+    event.create('kubejs:compressed_tar_black_licorice_3x')
+    event.create('kubejs:compressed_tar_black_licorice_4x')
+    
+    event.create('kubejs:compressed_sulfur_candy_apple_1x')
+    event.create('kubejs:compressed_sulfur_candy_apple_2x')
+    event.create('kubejs:compressed_sulfur_candy_apple_3x')
+    event.create('kubejs:compressed_sulfur_candy_apple_4x')    
+    
 })
 
 onEvent("minestuck.grist.registry", event => {
